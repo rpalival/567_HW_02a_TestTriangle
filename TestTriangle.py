acf9d6d2aa553e2d7fafa5276fea7e3962a0ff6f
Raj Palival <rajpalival@macbook-pro.mynetworksettings.com>
2022-10-08 04:46:17 -0400

    updated the scalene test case

--- TestTriangle.py
+++ TestTriangle.py
@@ -21,7 +21,7 @@ class TestTriangles(unittest.TestCase):
         self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
 
     def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        self.assertEqual(classifyTriangle(5,3,4),'Scalene','5,3,4 is a Right Scalene triangle')
         
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
