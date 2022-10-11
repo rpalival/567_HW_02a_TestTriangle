a8ada09b6f1d9c65f926e12b89461bd155bef52a
rpalival <rpalival@stevens.edu>
2022-10-11 19:26:42 -0400

    forgot to change the calling function name in test

--- TestTriangle.py
+++ TestTriangle.py
@@ -18,19 +18,19 @@ class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        self.assertEqual(classify_triangle(3,4,5),'Right','3,4,5 is a Right triangle')
 
     def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Scalene','5,3,4 is a Right Scalene triangle')
+        self.assertEqual(classify_triangle(5,3,4),'Scalene','5,3,4 is a Right Scalene triangle')
         
     def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classify_triangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
     
     def testRightTriangleA1(self):
-        self.assertEqual(classifyTriangle(5,12,13),'Right','5,12,13 is a Right triangle')
+        self.assertEqual(classify_triangle(5,12,13),'Right','5,12,13 is a Right triangle')
     
     def testInvalidInput(self):
-        self.assertEqual(classifyTriangle(3,4,8),'NotATriangle','3,4,8 is not a triangle')
+        self.assertEqual(classify_triangle(3,4,8),'NotATriangle','3,4,8 is not a triangle')
 
 if __name__ == '__main__':
     print('Running unit tests')
